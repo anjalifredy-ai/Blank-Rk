a70643e3a7da0e60c87e7037b7b3a13746e9f81c
Remon Francis <anjalifredy@gmail.com>
2026-08-31 15:44:58 +0530

    Create RetrofitClient.kt

---
+++ app/src/main/java/com/blankrk/app/data/network/RetrofitClient.kt
@@ -0,0 +1,28 @@
+package com.blankrk.app.data.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BASIC
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val youTubeApiService: YouTubeApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(YouTubeApiService::class.java)
+    }
+}
